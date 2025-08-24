8892f841326ffc787a8c933ae5d9021e8c99676d
Jacoo <yokobe0012@gmail.com>
2025-08-24 14:38:32 +0800

    更新连接

--- trunk/Dev/Assets/JuiceZombies/Scripts/HotFix/UI/Protocol/Protocol/Class/MyUrl.cs
+++ trunk/Dev/Assets/JuiceZombies/Scripts/HotFix/UI/Protocol/Protocol/Class/MyUrl.cs
@@ -2,7 +2,7 @@
 {
     public static class MyUrl
     {
-        public static string urlipv4 = "26.201.56.51:7176";
+        public static string urlipv4 = "192.168.123.44:7176";
         //public static string urlipv4 = "[fe80::f8c7:168:70f5:1b95%8]:5159";
         public static string urlipv4pro = "192.168.2.112:7176";
         public static string urlipv6 = "192.168.28.112:5159";
